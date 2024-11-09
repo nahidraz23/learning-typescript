4ab7c36da9023a92f3c2b0754cd7e44be923a697
nahidraz23 <nzamanraz@gmail.com>
2024-11-09 20:37:42 +0600

    problem-3 solved

--- Day_1/solutions/2.ts
+++ Day_1/solutions/2.ts
@@ -1,4 +1,12 @@
 {
+
+    /*
+    ----------------------
+    problem - 2
+    ----------------------
+    Create a Product object that should be immutable after its initial creation. You need to be able to read the properties but should not allow modifications once created. Use TypeScript's Readonly utility type.
+     */
+
     type ProductType = {
         name: string,
         readonly price: number
---
+++ Day_1/solutions/3.ts
@@ -0,0 +1,35 @@
+{
+  /*
+  -------------------
+  problem - 3
+  ------------------
+Write a function handleResponse that handles various API responses. The response can either be a success message (string), an error message (string), or an object with statusCode (number) and details (string). Based on the response type, the function should behave differently.
+*/
+
+  type ResponseType = string | { statusCode: number; details: string };
+
+  //   interface ResponseType1 {
+  //     statusCode: number;
+  //     details: string;
+  //   }
+
+  //   const success = "";
+
+    const message: ResponseType = {
+      statusCode: 401,
+      details: "successfull",
+    };
+
+//   const value = 10; //this will show error
+
+  const handleResponse = (res: ResponseType): boolean => {
+    if (res) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const result = handleResponse(message);
+  console.log(result);
+}
